f789c482f94156505709a40226fe3d6ec66fa75c
Evgeny Lyutikov <elyutikov@vivid.money>
2022-07-20 19:42:09 +0300

    aoc2020d01 refactoring

--- aoc2020/src/d01.rs
+++ aoc2020/src/d01.rs
@@ -1,9 +1,10 @@
 // https://adventofcode.com/2020/day/1
 
-use std::{collections::HashSet, fs};
+use std::fs;
+use itertools::Itertools;
 
 #[allow(dead_code)]
-fn main_d01() -> Result<i64, String> {
+fn main_d01() -> Result<(i64, i64), String> {
   let target = 2020;
   fs::read_to_string("inputs/d01.txt")
     .map_err(|error| error.to_string())
@@ -14,26 +15,23 @@ fn main_d01() -> Result<i64, String> {
         dbg!(errors);
       }
 
-      find_solution(numbers, target)
+      Option::zip(
+        find_2_number(numbers.clone(), target).map(|(a, b)| a * b),
+        find_3_numbers(numbers, target).map(|(a, b, c)| a * b * c),
+      )
+      .ok_or("unable to find solutions".to_string())
     })
 }
 
-fn find_solution(numbers: Vec<i64>, target: i64) -> Result<i64, String> {
-  let set: HashSet<i64> = HashSet::from_iter(numbers.iter().cloned());
+fn find_2_number(numbers: Vec<i64>, target: i64) -> Option<(i64, i64)> {
+  numbers.into_iter().tuple_combinations().find(|(a, b)| a + b == target)
+}
 
-  let result: Vec<(i64, &i64)> = numbers
+fn find_3_numbers(numbers: Vec<i64>, target: i64) -> Option<(i64, i64, i64)> {
+  numbers
     .into_iter()
-    .flat_map(|n| {
-      let y = target - n;
-      set.get(&y).map(|f| (n, f))
-    })
-    .collect();
-
-  match result[..] {
-    [(num1, num2), _] => Ok(num1 * num2),
-    [_, _, ..] => Err(format!("more than one solution found: {:?}", result).to_string()),
-    _ => Err("unable to find solution".to_string()),
-  }
+    .tuple_combinations()
+    .find(|(a, b, c)| a + b + c == target)
 }
 
 fn parse_vectors(input: String, separator: &str) -> (Vec<i64>, Vec<String>) {
@@ -73,18 +71,29 @@ mod test {
   }
 
   #[test]
-  fn find_solution_works() {
+  fn find_2_number_works() {
     let numbers = vec![1721, 979, 366, 299, 675, 1456];
     let target = 2020;
     let expected = 514579;
-    let result = find_solution(numbers, target);
+    let result = find_2_number(numbers, target);
 
-    assert_eq!(result, Ok(expected));
+    assert_eq!(result.map(|a| a.0 * a.1), Some(expected));
+  }
+
+  #[test]
+  fn find_3_number_works() {
+    let numbers = vec![1721, 979, 366, 299, 675, 1456];
+    let target = 2020;
+    let expected = 241861950;
+    let result = find_3_numbers(numbers, target);
+
+    assert_eq!(result.map(|a| a.0 * a.1 * a.2), Some(expected));
   }
 
   #[test]
   fn main_works() {
+    let expected = (1015476, 200878544);
     let result = main_d01();
-    assert_eq!(result, Ok(1015476));
+    assert_eq!(result, Ok(expected));
   }
 }
